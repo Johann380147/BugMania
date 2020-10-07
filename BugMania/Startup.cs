4be237612efdb8ca1f8cffad330a765def3f22b4
Johann380147 <holygrail49@hotmail.com>
2020-10-07 17:28:00 +0800

    Add project files.

---
+++ BugMania/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugMania.Startup))]
+namespace BugMania
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
