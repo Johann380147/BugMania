9a0e639e31fd32bb4e4f4d665b6860e141115f6b
Johann380147 <holygrail49@hotmail.com>
2020-10-18 15:01:44 +0800

    Only Admins can edit everything, certain other roles can only modify the Status. Developers can only change Status to "UNVERIFIED_FIXED". Triagers can only change Status to "NEW,ASSIGNED,INVALID,DUPLICATE". Reviewers can only change Status to "UNVERIFIED_FIXED, VERIFIED_FIXED". User and Reporter cannot modify anything.

--- BugMania/Controllers/BugReport/EditBugReportController.cs
+++ BugMania/Controllers/BugReport/EditBugReportController.cs
@@ -41,11 +41,31 @@ namespace BugMania.BugReportControllers
                 return HttpNotFound();
             }
 
-            ViewBag.PriorityId = new SelectList(priorityEntity.GetAllPriorities(), "Id", "Name", bugReport.PriorityId);
-            ViewBag.SeverityId = new SelectList(severityEntity.GetAllSeverities(), "Id", "Name", bugReport.SeverityId);
-            ViewBag.ProductId = new SelectList(productEntity.GetAllProducts(), "Id", "Name", bugReport.ProductId);
-            ViewBag.StatusId = new SelectList(statusEntity.GetAllStatus(), "Id", "Name", bugReport.StatusId);
-            ViewBag.Tags = new SelectList(tagEntity.GetAllTags(), "Id", "Name");
+            var products = productEntity.GetAllProducts();
+            var severity = severityEntity.GetAllSeverities();
+            var priority = priorityEntity.GetAllPriorities();
+            SelectList ProductList;
+            SelectList SeverityList;
+            SelectList PriorityList;
+
+            if (User.IsInRole("Admin"))
+            {
+                ProductList = new SelectList(products, "Id", "Name", bugReport.ProductId);
+                SeverityList = new SelectList(severity, "Id", "Name", bugReport.SeverityId);
+                PriorityList = new SelectList(priority, "Id", "Name", bugReport.PriorityId);
+            }
+            else
+            {
+                ProductList = new SelectList(products, "Id", "Name", bugReport.ProductId, products.Select(t => t.Id));
+                SeverityList = new SelectList(severity, "Id", "Name", bugReport.SeverityId, severity.Select(t => t.Id));
+                PriorityList = new SelectList(priority, "Id", "Name", bugReport.PriorityId, priority.Select(t => t.Id));
+            }
+
+            ViewBag.ProductList = ProductList;
+            ViewBag.SeverityList = SeverityList;
+            ViewBag.PriorityList = PriorityList;
+            ViewBag.StatusList = GetSelectListItemsForRole(bugReport.Assignees, statusEntity, bugReport.StatusId); ;
+            //ViewBag.Tags = new SelectList(tagEntity.GetAllTags(), "Id", "Name");
 
             var viewModel = new EditBugReportViewModel(bugReport);
 
@@ -65,12 +85,95 @@ namespace BugMania.BugReportControllers
                 return Redirect("/Report/Details/" + editBugReportViewModel.Id);
             }
 
-            ViewBag.PriorityId = new SelectList(priorityEntity.GetAllPriorities(), "Id", "Name", editBugReportViewModel.PriorityId);
-            ViewBag.SeverityId = new SelectList(severityEntity.GetAllSeverities(), "Id", "Name", editBugReportViewModel.SeverityId);
-            ViewBag.ProductId = new SelectList(productEntity.GetAllProducts(), "Id", "Name", editBugReportViewModel.ProductId);
-            ViewBag.StatusId = new SelectList(statusEntity.GetAllStatus(), "Id", "Name", editBugReportViewModel.StatusId);
-            
+            var products = productEntity.GetAllProducts();
+            var severity = severityEntity.GetAllSeverities();
+            var priority = priorityEntity.GetAllPriorities();
+            SelectList ProductList;
+            SelectList SeverityList;
+            SelectList PriorityList;
+
+            if (User.IsInRole("Admin"))
+            {
+                ProductList = new SelectList(products, "Id", "Name", editBugReportViewModel.ProductId);
+                SeverityList = new SelectList(severity, "Id", "Name", editBugReportViewModel.SeverityId);
+                PriorityList = new SelectList(priority, "Id", "Name", editBugReportViewModel.PriorityId);
+            }
+            else
+            {
+                ProductList = new SelectList(products, "Id", "Name", editBugReportViewModel.ProductId, products.Select(t => t.Id));
+                SeverityList = new SelectList(severity, "Id", "Name", editBugReportViewModel.SeverityId, severity.Select(t => t.Id));
+                PriorityList = new SelectList(priority, "Id", "Name", editBugReportViewModel.PriorityId, priority.Select(t => t.Id));
+            }
+
+            ViewBag.ProductList = ProductList;
+            ViewBag.SeverityList = SeverityList;
+            ViewBag.PriorityList = PriorityList;
+            ViewBag.StatusList = GetSelectListItemsForRole(editBugReportViewModel.Assignees, statusEntity, editBugReportViewModel.StatusId); ;
+
+
             return View("/Views/BugReport/EditBugReportUI.cshtml", editBugReportViewModel);
+        }
+
+        private IEnumerable<SelectListItem> GetSelectListItemsForRole(ICollection<ApplicationUser> assignees, StatusEntity statusEntity, int selectedItem)
+        {
+            var list = statusEntity.GetAllStatus();
+            var selectItem = new List<SelectListItem>();
+
+            foreach (var item in list)
+            {
+                selectItem.Add(new SelectListItem
+                {
+                    Value = Convert.ToString(item.Id),
+                    Text = item.Name,
+                    Selected = (item.Id == selectedItem)
+                });
+            }
+            if (User.IsInRole("Admin"))
+            {
+                selectItem.First(t => t.Text == "NEW").Disabled = false;
+                selectItem.First(t => t.Text == "ASSIGNED").Disabled = false;
+                selectItem.First(t => t.Text == "UNVERIFIED_FIXED").Disabled = false;
+                selectItem.First(t => t.Text == "VERIFIED_FIXED").Disabled = false;
+                selectItem.First(t => t.Text == "INVALID").Disabled = false;
+                selectItem.First(t => t.Text == "DUPLICATE").Disabled = false;
+            }
+            else if (User.IsInRole("Triager"))
+            {
+                selectItem.First(t => t.Text == "NEW").Disabled = false;
+                selectItem.First(t => t.Text == "ASSIGNED").Disabled = false;
+                selectItem.First(t => t.Text == "UNVERIFIED_FIXED").Disabled = true;
+                selectItem.First(t => t.Text == "VERIFIED_FIXED").Disabled = true;
+                selectItem.First(t => t.Text == "INVALID").Disabled = false;
+                selectItem.First(t => t.Text == "DUPLICATE").Disabled = false;
+            }
+            else if (User.IsInRole("Developer") &&
+                assignees.FirstOrDefault(i => i.Id == User.Identity.GetUserId()) != null)
+            {
+                selectItem.First(t => t.Text == "NEW").Disabled = true;
+                selectItem.First(t => t.Text == "ASSIGNED").Disabled = true;
+                selectItem.First(t => t.Text == "UNVERIFIED_FIXED").Disabled = false;
+                selectItem.First(t => t.Text == "VERIFIED_FIXED").Disabled = true;
+                selectItem.First(t => t.Text == "INVALID").Disabled = true;
+                selectItem.First(t => t.Text == "DUPLICATE").Disabled = true;
+            }
+            else if (User.IsInRole("Reviewer"))
+            {
+                selectItem.First(t => t.Text == "NEW").Disabled = true;
+                selectItem.First(t => t.Text == "ASSIGNED").Disabled = true;
+                selectItem.First(t => t.Text == "UNVERIFIED_FIXED").Disabled = false;
+                selectItem.First(t => t.Text == "VERIFIED_FIXED").Disabled = false;
+                selectItem.First(t => t.Text == "INVALID").Disabled = true;
+                selectItem.First(t => t.Text == "DUPLICATE").Disabled = true;
+            }
+            else
+            {
+                foreach (var item in selectItem)
+                {
+                    item.Disabled = true;
+                }
+            }
+
+            return selectItem;
         }
     }
 }
